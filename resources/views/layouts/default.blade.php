276abc6b83d2d1a3c80061d2142e2e2382f9fbdb
Alexander Gordon <alexadg@bgsu.edu>
2016-08-31 13:52:06 -0400

    Add basic auth and default layout. Will definitely want to restyle, but adding the basics to get us kickstarted for the visual demo in a couple weeks.

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    @include('includes.head')
+    <body>
+        <div class="container">
+            <div class="content">
+
+                @yield('content')
+
+            </div>
+        </div>
+    </body>
+</html>
