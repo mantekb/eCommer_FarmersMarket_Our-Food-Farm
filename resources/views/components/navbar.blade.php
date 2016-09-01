24c69fb05c779e94edd67fee01df68cb8e0f0ca5
Alexander Gordon <alexadg@bgsu.edu>
2016-09-01 16:37:31 -0400

    Set more blade templates to default.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,14 +3,10 @@
 		<a href="/" class="brand-logo">OurFoodFarm</a>
 		<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
 		<ul id="nav-web" class="right hide-on-med-and-down">
-			<li><a href="/stands">stands</a></li>
-			<li><a href="/food">food</a></li>
-			<li><a href="/account">account</a></li>
+			@include('components.navbar-list')
 		</ul>
 		<ul id="nav-mobile" class="side-nav">
-			<li><a href="/stands">stands</a></li>
-			<li><a href="/food">food</a></li>
-			<li><a href="/account">account</a></li>
+			@include('components.navbar-list')
 		</ul>
 	</div>
 </nav>
