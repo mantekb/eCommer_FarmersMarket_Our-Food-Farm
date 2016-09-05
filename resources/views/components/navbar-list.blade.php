1ca8c37e22405e5b97f38464857061b0f00984ed
Alexander Gordon <alexadg@bgsu.edu>
2016-09-05 15:18:45 -0400

    Learning Resources link

--- resources/views/components/navbar-list.blade.php
+++ resources/views/components/navbar-list.blade.php
@@ -1,6 +1,6 @@
 
-<li><a href="/stands">Stands</a></li>
-<li><a href="/food">Food</a></li>
+{{-- <li><a href="/food">Food</a></li> --}}
+<li><a href="/learning">Learning Resources</a></li>
 @if(Auth::guest())
 	<li><a href="/login">Login</a></li>
 	<li><a href="/register">Register</a></li>
