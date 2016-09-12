115f762de59eaf55998d2677430eeafb31b55d4b
Alec <alexadg@bgsu.edu>
2016-09-12 16:45:37 -0400

    Add create stand link on navbar.

--- resources/views/components/navbar-list.blade.php
+++ resources/views/components/navbar-list.blade.php
@@ -12,10 +12,10 @@
 //This should hopefully be unique between 2 calls.
 $identity = rand(0, 1000);
 ?>
-<li><a class="dropdown-button" data-beloworigin="true" data-activates='account-{{$identity}}'>{{ Auth::user()->name }}</a></li>
+<li><a class="dropdown-button" data-beloworigin="true" data-activates='account-{{$identity}}'>Account</a></li>
     <ul id="account-{{$identity}}"  class='dropdown-content'>
-      {{-- <li><a href="/friends">Friends</a></li> --}}
-      <li><a href="{{url("/settings")}}">Settings</a></li>
-      <li><a href="{{url("/logout")}}">Logout</a></li>
+	    <li><a href="{{url("/stand/create")}}">Create Stand</a></li>
+		<li><a href="{{url("/settings")}}">Settings</a></li>
+		<li><a href="{{url("/logout")}}">Logout</a></li>
     </ul>
 @endif
