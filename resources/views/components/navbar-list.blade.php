e50db5e714d0802fc5f5ae112e87f9fb6ab76111
Alec <alexadg@bgsu.edu>
2016-09-12 15:53:00 -0400

    Add Logout route This will make it so you can log in and out. #1 was about being able to register and setting up the database, which is already completed and was a pre-req for this. Closes #11

--- resources/views/components/navbar-list.blade.php
+++ resources/views/components/navbar-list.blade.php
@@ -6,5 +6,16 @@
 	<li><a href="{{url("/login")}}">Login</a></li>
 	<li><a href="{{url("/register")}}">Register</a></li>
 @else
-	<li><a href="{{url("/account")}}">Account</a></li>
+<?php
+//This is to make sure the dropdown works on mobile and desktop.
+//->Can't be having 2 entities with the same ID
+//This should hopefully be unique between 2 calls.
+$identity = rand(0, 1000);
+?>
+<li><a class="dropdown-button" data-beloworigin="true" data-activates='account-{{$identity}}'>{{ Auth::user()->name }}</a></li>
+    <ul id="account-{{$identity}}"  class='dropdown-content'>
+      {{-- <li><a href="/friends">Friends</a></li> --}}
+      <li><a href="{{url("/settings")}}">Settings</a></li>
+      <li><a href="{{url("/logout")}}">Logout</a></li>
+    </ul>
 @endif
