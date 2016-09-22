da2fb30732adcc0f8b563de9bc66b5264cda590a
Alec <alexadg@bgsu.edu>
2016-09-22 17:10:43 -0400

    Make window work with mobile References #27

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,7 @@
 <head>
     <title>@yield('title')</title>
-
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
     {{-- Materializecss.com --}}
 	<!-- Compiled and minified CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
