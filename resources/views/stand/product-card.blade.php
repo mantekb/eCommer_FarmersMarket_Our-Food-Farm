62129c0af13471e066482e96d838511c04005e3a
Sam <soneal674@gmail.com>
2016-10-19 08:36:06 -0400

    Minor Bug Fix: Tomato -Tomato image on deals and for product cards was not loading -downloaded the image and put it in public/img/

--- resources/views/stand/product-card.blade.php
+++ resources/views/stand/product-card.blade.php
@@ -1,7 +1,7 @@
 <div class="col s8 push-s2 m4 l3">
     <div class="card">
         <div class="card-image">
-            <img src="http://feelgrafix.com/data_images/out/27/956607-tomato.jpg" alt="">
+            <img src='{{asset("img/tomato.jpg")}}' alt="">
             <span class="card-title blue-text">{{$product->name or 'Tomato'}}</span>
         </div>
         <div class="card-content">
