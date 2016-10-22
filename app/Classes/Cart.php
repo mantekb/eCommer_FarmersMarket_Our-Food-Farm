e67f5b55cc707dcf583dd4f9ed7932a0b8f3fb0b
Alec <alexadg@bgsu.edu>
2016-10-21 21:00:00 -0400

    Improve Cart object - Add getIndex() function in order to reduce code reuse on Cart.   - This simplifies the add function and any future functions signifigantly by not having to re-write a search every time. - Add remove function to completely remove a product from your cart. - Everything works, but doesn't have a tie in yet. References #2

--- app/Classes/Cart.php
+++ app/Classes/Cart.php
@@ -13,30 +13,67 @@ class Cart
 
 
 	/**
-	* 
+	* Handle adding something to the cart.
+	* Either add a new item, or increment the quantity of an existing item.
 	*
-	* @
+	* @param $product - Product object, we add the quantity here.
+	* @param $quantity - How many of the product we want.
 	*/
 	public function add($product, $quantity)
 	{
-		$product->quantity = $quantity;
-		//See if the product is in the cart
-		$isFound = false;
+		$index = $this->getIndex($product);
+		if ($index == -1)
+		{
+			//Set the quantity of the new item.
+			$product->quantity = $quantity;
+			//If the part isn't in the cart yet, just add it in.
+			$this->members[] = $product;
+		}
+		else
+		{
+			//If the part is in the cart, increment the quantity
+			$this->members[$index]->quantity += $quantity;
+		}
+	}
+
+	/**
+	* Completely remove an item from the cart.
+	*
+	* @param $product - The Product we are removing.
+	*/
+	public function remove($product)
+	{
+		//Denotes if the object has not been removed.
+		$removed = false;
+		$index = $this->getIndex($product);
+		if ($index != -1)
+		{
+			//Remove item from the cart at $index, only 1 item.
+			array_splice($this->members, $index, 1);
+			$removed = true;
+		}
+		return $removed;
+	}
+
+	/**
+	* Obtain the index of a product.
+	*
+	* @param $product - What we are searching for.
+	*/
+	public function getIndex($product)
+	{
+		//-1 if not found
+		$index = -1;
 		$i = 0;
 		$numMembers = count($this->members);
-		while ($i < $numMembers && !$isFound)
+		while ($i < $numMembers && $index == -1)
 		{
 			if ($this->members[$i]->id == $product->id)
 			{
-				//Increment Quantity for Producct
-				$isFound = true;
-				$this->members[$i]->quantity += $quantity;
+				$index = $i;
 			}
 			$i++;
 		}
-		if (!$isFound)
-		{
-			$this->members[] = $product;
-		}
+		return $index;
 	}
 }
