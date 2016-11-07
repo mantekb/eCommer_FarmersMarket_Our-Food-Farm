d8f6cd22950c537594430149c6b14ddd8e0d4118
Sam <soneal674@gmail.com>
2016-11-07 16:53:14 -0500

    Add table and migration: -Created new model and migration for payment info --Unable to test yet, since no processing is implemented -#issue_20

---
+++ database/migrations/2016_11_07_211702_create_payment_infos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_infos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned();
+            $table->string('stripe_id');
+            $table->string('secret_key');
+            $table->string('publishable_key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('payment_infos');
+    }
+}
