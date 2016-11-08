c429599f375a33e31790328957f0559749708fb7
Sam <soneal674@gmail.com>
2016-11-08 17:24:44 -0500

    Commit for in progress changes: -Made corrections and edits to paymentinfo.blade -Finished payment.js -Started a function in PaymentController #issue_20

--- public/js/payment.js
+++ public/js/payment.js
@@ -10,31 +10,39 @@ if (window.location.href == DOCUMENT_ROOT+'/payment')
 	});
 }
 
-$('#submitpaymentinfo').on('click', function(e) {
+$('#submitpayinfo').on('click', function(e) {
 	e.preventDefault();
 	var firstname = $('#firstname').val();
 	var lastname = $('#lastname').val();
+	var cardNumber = $('#cardNumber').val();
+	var expDate = $('#expDate').val();
+	var cvc = $('#cvc').val();
 	var DOB = $('#DOB').val();
 	var address = $('#address').val();
 	var city = $('#city').val();
 	var state = $('#state').val();
 	var zip = $('#zip').val();
 
-	
-
-	// $.ajax({
-	//     url: DOCUMENT_ROOT+'/settings/name',
-	//     type: 'POST',
-	//     data: {
-	//     	name: name,
-	//     },
-	//     error: (response) => {
-	//         swal('Error', "Unable to change name.");
-	//     },
-	//     success: (response) => {
-	//         swal('Success', "Your name has changed to "+name+".");
-	//         $('#name-account-main').html(name);
-	//         $('#name-account-mobile').html(name);
-	//     }
-	// });
+	$.ajax({
+	    url: DOCUMENT_ROOT+'/payment/createStripeAccount',
+	    type: 'POST',
+	    data: {
+	    	firstname: firstname,
+	    	lastname: lastname,
+	    	cardNumber: cardNumber,
+	    	expDate: expDate,
+	    	cvc: cvc,
+	    	DOB: DOB,
+	    	address: address,
+	    	city: city,
+	    	state: state,
+	    	zip: zip,
+	    },
+	    error: (response) => {
+	        swal('Error', "Unable to create Stripe account.");
+	    },
+	    success: (response) => {
+	        swal('Success', "Your stripe account has been created.");
+	    }
+	});
 });
